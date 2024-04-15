7e61e696a5e2bbeb0ddfbf3eaeda8f2e1b48677a
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-04-15 00:17:21 -0300

    feat: create home view model with state

---
+++ app/src/main/java/br/com/dio/picpaycloneapp/ui/screens/home/HomeViewModel.kt
@@ -0,0 +1,23 @@
+package br.com.dio.picpaycloneapp.ui.screens.home
+
+import androidx.lifecycle.ViewModel
+import br.com.dio.picpaycloneapp.data.Balance
+import br.com.dio.picpaycloneapp.data.Transaction
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor() : ViewModel() {
+
+    private val _state = MutableStateFlow(HomeUiState())
+    val state: StateFlow<HomeUiState> = _state
+}
+
+data class HomeUiState(
+    val isLoadingBalance: Boolean = false,
+    val isLoadingTransactions: Boolean = false,
+    val balance: Balance? = null,
+    val transactions: List<Transaction> = listOf()
+)
